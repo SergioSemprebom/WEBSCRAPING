f540ddc8a374d65fb4dff5473c420da85e061006
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-11-16 20:39:46 -0300

    Atualizando Vendas

--- mercadolivre.py
+++ mercadolivre.py
@@ -1,10 +1,24 @@
 import scrapy
 
-
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
-    allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
 
     def parse(self, response):
-        pass
+        products = response.css('div.poly-card__content')
+        #products = response.css('poly-component__variations')
+
+
+        for product in products:
+
+            yield {
+                # LEVE STAR
+                'brand': products.css('span.poly-component__brand::text').get(),
+                #'brand': products.css(f'h2.{tenis}').get()
+
+                # OLYMPIKUS
+                #'name': products.css('ui-search-main ui-search-main--only-products.ui-search-main--with-topkeywords::text').get
+            }
+
+
+                
