2afeb8ee95d6f657706270fe4af84550c6ec02cf
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-11-16 03:03:41 -0300

    Analise de vendas 2.0

---
+++ coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+
+    def parse(self, response):
+        products = response.css('div.poly-card__content')
+
+        for product in products:
+
+            'brand' = products.css('span.poly-component__brand::text').get()
